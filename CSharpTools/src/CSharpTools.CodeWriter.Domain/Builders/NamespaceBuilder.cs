61d4131352d334b885f19c5fe515a032084aec73
Willem Melis <willem.melis@student.kuleuven.be>
2019-02-23 15:59:44 +0100

    simplified namespace generation

--- CSharpTools/src/CSharpTools.CodeWriter.Domain/Builders/NamespaceBuilder.cs
+++ CSharpTools/src/CSharpTools.CodeWriter.Domain/Builders/NamespaceBuilder.cs
@@ -12,18 +12,6 @@ namespace CSharpTools.CodeWriter.Domain.Builders
     public class NamespaceBuilder : IBuilder<NamespaceDeclarationSyntax, List<string>>
     {
         public NamespaceDeclarationSyntax Build(List<string> input)
-        {
-            // temp hack 
-            var namespaceFullNameLocation = String.Join(".",input.Take(input.Count() - 1));
-            var namespaceName = input.Last();
-
-            NameSyntax namespaceSyntaxName = (input.Count < 2) 
-                ?  SyntaxFactory.IdentifierName(namespaceName)
-                :  (NameSyntax) SyntaxFactory.QualifiedName(
-                    SyntaxFactory.ParseName(namespaceFullNameLocation),
-                    SyntaxFactory.IdentifierName(namespaceName));
-
-            return SyntaxFactory.NamespaceDeclaration(namespaceSyntaxName);
-        }
+            => SyntaxFactory.NamespaceDeclaration(SyntaxFactory.ParseName(String.Join(".", input)));
     }
 }
